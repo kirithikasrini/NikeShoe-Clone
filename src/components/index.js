63b8c1a39aed88af3cf57fe4683df2d8441f57b5
kirithikasrini <kirithikasrini04@gmail.com>
2024-11-23 21:55:33 +0530

    initial

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularCard from "./PopularCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularCard,
+    ServiceCard,
+    ReviewCard,
+}
---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from './CustomerReview';
+import Hero from './Hero';
+import Footer from './Footer';
+import Popular from './Popular';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    CustomerReview,
+    Footer,
+    Popular,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe   
+}
